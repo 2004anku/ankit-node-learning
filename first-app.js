6b212dba9de93508c9b98504b7c8e19ee7388c64
Ankit choudhary <ankitjat3232@gmail.com>
2026-05-02 21:17:26 -0400

    Todays new learing

--- first-app.js
+++ first-app.js
@@ -1,8 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log(res.body);
   res.write("Hello Ankit");
   res.end(0);
 });
 
 server.listen(3000);
+//  new section started form momday
