cfdec1a71d0b7613236a69a6eef586bdc3c6769d
Ankit choudhary <ankitjat3232@gmail.com>
2026-05-15 21:01:02 -0400

    updated

--- features/user/user.controller.js
+++ features/user/user.controller.js
@@ -19,7 +19,7 @@ const createUser = async (req, res) => {
   }
 };
 
-// GET ALL USERS
+// GET
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
@@ -33,77 +33,19 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-// GET SINGLE USER
-const getSingleUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
-
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json({
-      message: "Error fetching user",
-      error: err.message,
-    });
-  }
-};
-
-// UPDATE USER
-const updateUser = async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-
-    if (!updatedUser) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
-
-    res.status(200).json({
-      message: "User updated successfully",
-      data: updatedUser,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: "Error updating user",
-      error: err.message,
-    });
-  }
-};
-
-// DELETE USER
+// DELETE
 const deleteUser = async (req, res) => {
   try {
-    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    await User.findByIdAndDelete(req.params.id);
 
-    if (!deletedUser) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
-
-    res.status(200).json({
-      message: "User deleted successfully",
-    });
+    res.send("User deleted successfully");
   } catch (err) {
-    res.status(500).json({
-      message: "Error deleting user",
-      error: err.message,
-    });
+    res.send(err);
   }
 };
 
 module.exports = {
   createUser,
   getAllUsers,
-  getSingleUser,
-  updateUser,
   deleteUser,
 };
--- features/user/user.routes.js
+++ features/user/user.routes.js
@@ -10,9 +10,6 @@ router.post("/register", userController.createUser);
 // GET USERS
 router.get("/all", userController.getAllUsers);
 
-// UPDATE USER
-router.put("/update/:id", userController.updateUser);
-
 // DELETE USER
 router.delete("/delete/:id", userController.deleteUser);
 
